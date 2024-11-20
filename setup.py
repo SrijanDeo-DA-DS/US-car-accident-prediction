f85e96303b30986d11a14f5c494b76a57d9c0dfc
SrijanDeo-DA-DS <sd759@njit.edu>
2024-11-19 20:19:00 -0600

    init project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project uses The US-Accidents dataset can be used for numerous applications, such as real-time car accident prediction, studying car accident hotspot locations, casualty analysis, extracting cause and effect rules to predict car accidents, and studying the impact of precipitation or other environmental stimuli on accident occurrence.',
+    author='SrijanDeo',
+    license='MIT',
+)
